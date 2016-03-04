61122988956dafab056b0330df4343df3e132767
honghao <honghao.zheng@imhuihui.com>
2016-03-04 10:51:59 +0800

    init HGReader

---
+++ HGReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGReader
+//
+//  Created by zhh on 16/3/4.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HGReaderTests/HGReaderTests.m
@@ -0,0 +1,39 @@
+//
+//  HGReaderTests.m
+//  HGReaderTests
+//
+//  Created by zhh on 16/3/4.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HGReaderTests : XCTestCase
+
+@end
+
+@implementation HGReaderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
