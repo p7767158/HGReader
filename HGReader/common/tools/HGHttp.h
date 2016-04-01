50dfc35a1e53e3442b3352fe7295bceff35220a3
honghao <honghao.zheng@imhuihui.com>
2016-04-01 14:26:31 +0800

    test-internet

---
+++ HGReader/common/tools/HGHttp.h
@@ -0,0 +1,13 @@
+//
+//  HGHttp.h
+//  HGReader
+//
+//  Created by zhh on 16/3/24.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HGHttp : NSObject
+
+@end
---
+++ HGReader/common/tools/HGHttp.m
@@ -0,0 +1,15 @@
+//
+//  HGHttp.m
+//  HGReader
+//
+//  Created by zhh on 16/3/24.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import "HGHttp.h"
+#import "AFNetworking.h"
+
+@implementation HGHttp
+
+
+@end
